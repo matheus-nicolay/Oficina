3f5dc106cefb1e9163b7cc1c080d8dcf548d4412
Matheus Vieira Nicolay <05086877059@FW.IFFAR.EDU.BR>
2023-02-28 21:47:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.aula1',
+  appName: 'aula1',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
