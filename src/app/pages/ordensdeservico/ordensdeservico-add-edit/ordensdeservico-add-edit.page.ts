2ccd1727fa27575337b24c7ec81da6ceeec94c04
Matheus <nicolaymatheus222@gmail.com>
2023-06-06 16:54:04 -0300

    Edited ordensdeservico

--- src/app/pages/ordensdeservico/ordensdeservico-add-edit/ordensdeservico-add-edit.page.ts
+++ src/app/pages/ordensdeservico/ordensdeservico-add-edit/ordensdeservico-add-edit.page.ts
@@ -6,6 +6,10 @@ import { Cliente } from 'src/app/models/cliente.model';
 import { ClientesService } from 'src/app/services/clientes.service';
 import { DatePicker } from '@ionic-native/date-picker/ngx';
 import { Platform } from '@ionic/angular';
+import { OrdernsDeServicoService } from 'src/app/services/ordensdeservico.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastService } from 'src/app/services/toast.service';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   templateUrl: './ordensdeservico-add-edit.page.html'
@@ -22,30 +26,44 @@ export class OrdensDeServicoAddEditPage implements OnInit {
     private clientesService: ClientesService,
     private datePicker: DatePicker,
     private platform: Platform,
+    private route: ActivatedRoute,
+    private ordensDeServicoService: OrdernsDeServicoService,
+    private toastService: ToastService,
+    private alertService: AlertService,
+    private router: Router
   ) { }
 
   async ngOnInit() {
   }
 
   async ionViewWillEnter() {
+    const id = this.route.snapshot.paramMap.get('id');
     const clientes = await this.clientesService.getAll();
     this.clientes = clientes;
-    this.ordemDeServico = {
-      ordemdeservicoid: Guid.createEmpty().toString(),
-      clienteid: Guid.createEmpty().toString(),
-      veiculo: '',
-      dataehoraentrada: new Date()
-    };
-    this.modoDeEdicao = true;
-    this.osForm = this.formBuilder.group({
-      ordemdeservicoid: [this.ordemDeServico.ordemdeservicoid],
-      clienteid: [this.ordemDeServico.clienteid, Validators.required],
-      veiculo: [this.ordemDeServico.veiculo, Validators.required],
-      dataentrada: [{ value: this.ordemDeServico.dataehoraentrada.toLocaleDateString(), disabled: !this.modoDeEdicao }, Validators.required],
-      horaentrada: [{ value: this.ordemDeServico.dataehoraentrada.toLocaleTimeString(), disabled: !this.modoDeEdicao }, Validators.required],
-      dataehoraentrada: [this.ordemDeServico.dataehoraentrada]
-    });
+
+    if(id != null){
+      const isIdEmptyGUID = Guid.parse(id).isEmpty();
+      const isIdValidGUID = Guid.isGuid(id);
+
+      if (id && !isIdEmptyGUID && isIdValidGUID){
+        this.ordemDeServico = await this.ordensDeServicoService.getByid(id);
+      }else{
+        this.ordemDeServico = {ordemdeservicoid: Guid.createEmpty().toString(),clienteid: Guid.createEmpty().toString(), veiculo: '', dataehoraentrada: new Date()};
+        this.modoDeEdicao = true;
+      }
+
+      this.osForm = this.formBuilder.group({
+        ordemdeservicoid: [this.ordemDeServico.ordemdeservicoid],
+        clienteid: [this.ordemDeServico.clienteid, Validators.required],
+        veiculo: [this.ordemDeServico.veiculo, Validators.required],
+        dataentrada: [{ value: this.ordemDeServico.dataehoraentrada.toLocaleDateString(), disabled: !this.modoDeEdicao }, Validators.required],
+        horaentrada: [{ value: this.ordemDeServico.dataehoraentrada.toLocaleTimeString(), disabled: !this.modoDeEdicao }, Validators.required],
+        dataehoraentrada: [this.ordemDeServico.dataehoraentrada]
+      });
+      this.ordemDeServico = this.osForm.value;
+    }
   }
+
   selecionarDataEntrada() {
     if (!this.modoDeEdicao) {
       return;
@@ -98,4 +116,34 @@ export class OrdensDeServicoAddEditPage implements OnInit {
       }
     });
   }
+
+  iniciarEdicao(){
+    this.modoDeEdicao = true;
+  }
+
+  cancelarEdicao(){
+    this.osForm.setValue(this.ordemDeServico);
+    this.modoDeEdicao = false;
+  }
+
+  async submit() {
+    if (this.osForm.invalid || this.osForm.pending) {
+      await this.alertService.presentAlert('Falha', 'Gravação não foi executada', 'Verifique os dados informados para o atendimento', ['Ok']);
+      return;
+    }
+    const dataString = new Date(this.osForm.controls['dataentrada'].value).toDateString();
+    const horaString = new Date(this.osForm.controls['horaentrada'].value).toTimeString();
+    const dataEHora = new Date(dataString + ' ' + horaString);
+
+    await this.ordensDeServicoService.update(
+      {
+        ordemdeservicoid: this.osForm.controls['ordemdeservicoid'].value,
+        clienteid: this.osForm.controls['clienteid'].value,
+        veiculo: this.osForm.controls['veiculo'].value,
+        dataehoraentrada: dataEHora,
+      }
+    );
+    this.toastService.presentToast('Gravação bem sucedida', 3000, 'top');
+    this.router.navigateByUrl('ordensdeservico-listagem');
+  } 
 }
--- src/app/services/ordensdeservico.service.ts
+++ src/app/services/ordensdeservico.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { DatabaseService } from "./database.service";
 import { OrdemDeServico } from "../models/ordemdeservico.model";
+import { databaseName } from "./database.statements";
 
 @Injectable({
     providedIn: 'root'
@@ -25,5 +26,53 @@ export class OrdernsDeServicoService{
             }
         }
         return ordensdeservico;
+    }
+
+    public async getByid (id: string ): Promise<any> {
+        try {
+            const db = await this.databaseService.sqliteConnection.retrieveConnection(databaseName, false);
+            const sql = 'select * from ordensdeservico where ordemdeservico id = ?';
+
+            try{
+                db.open();
+                const data = await db.query(sql, [id]);
+                db.close();
+                if (data.values!.length > 0){
+                    const ordemdeservico: OrdemDeServico = data.values![0];
+                    ordemdeservico.dataehoraentrada = new Date(ordemdeservico.dataehoraentrada);
+                    return ordemdeservico;
+                }else{
+                    return null;
+                }
+            } catch (e) {
+                return console.error(e);
+            }
+        } catch (e) {
+            return console.error(e);
+        }
+    
+    }
+
+    async update(ordemdeservico: OrdemDeServico): Promise<void> {
+        let sql: any;
+        let params: any;
+
+        if(Guid.parse(ordemdeservico.ordemdeservicoid).isEmpty()){
+            ordemdeservico.ordemdeservicoid = Guid.create().toString();
+            sql = 'INSERT INTO ordensdeservico(ordemdeservicoid, clienteid, veiculo, dataehoranetrada) ' + 'values(?, ?, ?, ?)';
+            params = [ordemdeservico.ordemdeservicoid, ordemdeservico.clienteid, ordemdeservico.veiculo, ordemdeservico.dataehoraentrada];
+        }else{
+            sql = 'UPDATE ordensdeservico SET clienteid = ?, veiculo = ?, ' + 'dataehoraentrada = ? WHERE ordemdeservicoid = ?';
+            params = [ordemdeservico.clienteid, ordemdeservico.veiculo, ordemdeservico.dataehoraentrada, ordemdeservico.ordemdeservicoid];
+        }
+        
+        try {
+            const db = await this.databaseService.sqliteConnection.retrieveConnection(databaseName, false);
+            db.open();
+            await db.run(sql, params);
+            db.close();
+        } catch (e) {
+            console.error(e);
+        }
     }
 }
